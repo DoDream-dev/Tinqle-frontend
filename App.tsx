c28363d537a4ea0d02f79208423e0425b4f05eae
0xseo <0xseo@yonsei.ac.kr>
2023-09-05 00:05:25 +0900

    first setting

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {Text, TouchableHighlight, View} from 'react-native';
+import {useCallback} from 'react';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+
+function HomeScreen({navigation}: HomeScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  return (
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableHighlight onPress={onClick}>
+              <Text>Home Screen</Text>
+            </TouchableHighlight>
+          </View>
+  );
+}
+
+function DetailsScreen({navigation}: DetailsScreenProps) {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableHighlight onPress={onClick}>
+              <Text>Details Screen</Text>
+            </TouchableHighlight>
+          </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+function App() {
+  return (
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen
+                      name="Home"
+                      component={HomeScreen}
+                      options={{title: 'Overview'}}
+              />
+              <Stack.Screen name="Details">
+                {props => <DetailsScreen {...props} />}
+              </Stack.Screen>
+            </Stack.Navigator>
+          </NavigationContainer>
+  );
+}
+
+export default App;
