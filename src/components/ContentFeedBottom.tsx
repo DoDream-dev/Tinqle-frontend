00588f9f56df2772d2bb7eacab77649da8f565be
sjsjmine129 <sjsjmine129@gmail.com>
2024-01-17 14:55:54 +0900

    Fix : feed list ui
    피드 리스트 페이지의 컴포넌트 구조 변경
    시간 아래로
    글과 사진도 아래로

--- src/components/ContentFeedBottom.tsx
+++ src/components/ContentFeedBottom.tsx
@@ -1,88 +1,114 @@
-import React from "react";
-import { StyleSheet, View, Text, Pressable } from "react-native";
-import Emoticons from "./Emoticons";
-import { svgXml } from '../../assets/image/svgXml';
-import { SvgXml } from 'react-native-svg'
+import React from 'react';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import Emoticons from './Emoticons';
+import {svgXml} from '../../assets/image/svgXml';
+import {SvgXml} from 'react-native-svg';
 type EmoticonsProps = {
-  smileEmoticonCount:number;
-  sadEmoticonCount:number;
-  heartEmoticonCount:number;
-  surpriseEmoticonCount:number;
-  isCheckedSmileEmoticon:boolean;
-  isCheckedSadEmoticon:boolean;
-  isCheckedHeartEmoticon:boolean;
-  isCheckedSurpriseEmoticon:boolean;
-}
+  smileEmoticonCount: number;
+  sadEmoticonCount: number;
+  heartEmoticonCount: number;
+  surpriseEmoticonCount: number;
+  isCheckedSmileEmoticon: boolean;
+  isCheckedSadEmoticon: boolean;
+  isCheckedHeartEmoticon: boolean;
+  isCheckedSurpriseEmoticon: boolean;
+};
 type ContentFeedBottomProps = {
-  mine:boolean;
-  detail:boolean;
-  commentCnt:number;
-  emoticons:EmoticonsProps;
-  press:(feedId:number, emotion:string)=>Promise<void>;
-  feedId:number;
-  whoReact:(feedId:number)=>Promise<void>;
-}
-export default function ContentFeedBottom(props:ContentFeedBottomProps){
+  mine: boolean;
+  detail: boolean;
+  commentCnt: number;
+  emoticons: EmoticonsProps;
+  press: (feedId: number, emotion: string) => Promise<void>;
+  feedId: number;
+  whoReact: (feedId: number) => Promise<void>;
+};
+export default function ContentFeedBottom(props: ContentFeedBottomProps) {
   const mine = props.mine;
   const detail = props.detail;
   const commentCnt = props.commentCnt;
   const emoticons = props.emoticons;
-  
+
   const emotionData = {
-    heart:{pressed:emoticons.isCheckedHeartEmoticon, count:emoticons.heartEmoticonCount},
-    smile:{pressed:emoticons.isCheckedSmileEmoticon, count:emoticons.smileEmoticonCount},
-    sad:{pressed:emoticons.isCheckedSadEmoticon, count:emoticons.sadEmoticonCount},
-    surprise:{pressed:emoticons.isCheckedSurpriseEmoticon, count:emoticons.surpriseEmoticonCount}
-  }
+    heart: {
+      pressed: emoticons.isCheckedHeartEmoticon,
+      count: emoticons.heartEmoticonCount,
+    },
+    smile: {
+      pressed: emoticons.isCheckedSmileEmoticon,
+      count: emoticons.smileEmoticonCount,
+    },
+    sad: {
+      pressed: emoticons.isCheckedSadEmoticon,
+      count: emoticons.sadEmoticonCount,
+    },
+    surprise: {
+      pressed: emoticons.isCheckedSurpriseEmoticon,
+      count: emoticons.surpriseEmoticonCount,
+    },
+  };
   return (
     <View style={detail ? styles.entireDetail : styles.entire}>
-      {!detail && <View style={styles.feedListCommentView}>
-        <SvgXml width={20} height={20} xml={svgXml.icon.commentIcon} />
-        <Text style={styles.feedListCommentTxt}>{commentCnt}</Text>
-      </View>}
+      {!detail && (
+        <View style={styles.feedListCommentView}>
+          <SvgXml width={20} height={20} xml={svgXml.icon.commentIcon} />
+          <Text style={styles.feedListCommentTxt}>{commentCnt}</Text>
+        </View>
+      )}
       <View style={styles.emoticonView}>
-        <Emoticons mine={mine} emotionData={emotionData} press={props.press} feedId={props.feedId}/>
-        {mine && <Pressable style={styles.reactedPeopleBtn} onPress={()=>{props.whoReact(props.feedId)}}>
-          <SvgXml width={20} height={20} xml={svgXml.icon.reacted} />
-        </Pressable>}
+        <Emoticons
+          mine={mine}
+          emotionData={emotionData}
+          press={props.press}
+          feedId={props.feedId}
+        />
+        {mine && (
+          <Pressable
+            style={styles.reactedPeopleBtn}
+            onPress={() => {
+              props.whoReact(props.feedId);
+            }}>
+            <SvgXml width={20} height={20} xml={svgXml.icon.reacted} />
+          </Pressable>
+        )}
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  entire:{
-    flex:1,
+  entire: {
+    flex: 1,
+    paddingHorizontal: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  entireDetail:{
+  entireDetail: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'center',
     marginLeft: 32,
     marginRight: 10,
   },
-  feedListCommentView:{
+  feedListCommentView: {
     flexDirection: 'row',
-    alignItems:'center',
-    backgroundColor:'#202020',
-    paddingLeft:4,
-    paddingRight:6,
-    paddingVertical:4,
-    borderRadius:20
+    alignItems: 'center',
+    backgroundColor: '#202020',
+    paddingLeft: 4,
+    paddingRight: 6,
+    paddingVertical: 4,
+    borderRadius: 20,
   },
-  feedListCommentTxt:{
+  feedListCommentTxt: {
     color: '#888888',
     fontSize: 13,
     fontWeight: '500',
-    marginLeft: 2
+    marginLeft: 2,
   },
-  emoticonView:{
+  emoticonView: {
     flexDirection: 'row',
   },
-  reactedPeopleBtn:{
+  reactedPeopleBtn: {
     backgroundColor: '#202020',
     height: 28,
     borderRadius: 20,
@@ -90,5 +116,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     // marginRight: 16
-  }
-})
+  },
+});
--- src/components/Emoticons.tsx
+++ src/components/Emoticons.tsx
@@ -1,40 +1,69 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Emoticon from "./Emoticon";
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Emoticon from './Emoticon';
 
 type EmotionDataType = {
-  pressed:boolean;
-  count:number;
-}
+  pressed: boolean;
+  count: number;
+};
+
 type EmoticonsProps = {
-  mine:boolean;
-  emotionData:{
-    heart:EmotionDataType;
-    smile:EmotionDataType;
-    sad:EmotionDataType;
-    surprise:EmotionDataType;
+  mine: boolean;
+  emotionData: {
+    heart: EmotionDataType;
+    smile: EmotionDataType;
+    sad: EmotionDataType;
+    surprise: EmotionDataType;
   };
-  press:(feedId:number, emotion:string)=>Promise<void>;
-  feedId:number;
-}
-export default function Emoticons(props:EmoticonsProps){
+  press: (feedId: number, emotion: string) => Promise<void>;
+  feedId: number;
+};
+
+export default function Emoticons(props: EmoticonsProps) {
   const mine = props.mine;
   const emotionData = props.emotionData;
   return (
     <View style={styles.entire}>
-      <Emoticon emotion="heart" mine={mine} count={emotionData.heart.count} pressed={emotionData.heart.pressed} press={props.press} feedId={props.feedId}/>
-      <Emoticon emotion="smile" mine={mine} count={emotionData.smile.count} pressed={emotionData.smile.pressed} press={props.press} feedId={props.feedId}/>
-      <Emoticon emotion="sad" mine={mine} count={emotionData.sad.count} pressed={emotionData.sad.pressed} press={props.press} feedId={props.feedId}/>
-      <Emoticon emotion="surprise" mine={mine} count={emotionData.surprise.count} pressed={emotionData.surprise.pressed} press={props.press} feedId={props.feedId}/>
+      <Emoticon
+        emotion="heart"
+        mine={mine}
+        count={emotionData.heart.count}
+        pressed={emotionData.heart.pressed}
+        press={props.press}
+        feedId={props.feedId}
+      />
+      <Emoticon
+        emotion="smile"
+        mine={mine}
+        count={emotionData.smile.count}
+        pressed={emotionData.smile.pressed}
+        press={props.press}
+        feedId={props.feedId}
+      />
+      <Emoticon
+        emotion="sad"
+        mine={mine}
+        count={emotionData.sad.count}
+        pressed={emotionData.sad.pressed}
+        press={props.press}
+        feedId={props.feedId}
+      />
+      <Emoticon
+        emotion="surprise"
+        mine={mine}
+        count={emotionData.surprise.count}
+        pressed={emotionData.surprise.pressed}
+        press={props.press}
+        feedId={props.feedId}
+      />
     </View>
   );
-
 }
 
 const styles = StyleSheet.create({
-  entire:{
+  entire: {
     flexDirection: 'row',
     // marginRight:6,
-    backgroundColor:'red'
+    // backgroundColor: 'red',
   },
-})
+});
